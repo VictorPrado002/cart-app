82044e8cb1faa0c900c322c2ab6fb1dc35ae2c86
VictorPrado002 <victor000hugo10@gmail.com>
2026-05-18 02:15:41 -0600

    recomposition to useReducer

--- src/CartApp.jsx
+++ src/CartApp.jsx
@@ -1,46 +1,45 @@
-import { useState } from "react";
+import { useEffect, useReducer,  } from "react";
 import { CartView } from "./components/CartView";
 import { CatalogView } from "./components/CatalogView";
+import { itemsReducer } from "./reducer/itemsReduces";
+import { AddProductCart, DeleteProductCart, UpdateQuantityProductCart } from "./reducer/itemsAction";
 
 const initialCartItems= JSON.parse(sessionStorage.getItem('cart'))|| [];
 
 export const CartApp =() =>{
-    const [cartItems,setCartItems] =useState(initialCartItems);
+    const [cartItems,dispatch]=useReducer(itemsReducer,initialCartItems);
     
+    useEffect(()=>{
+        sessionStorage.setItem('cart',JSON.stringify(cartItems));
+    },[cartItems]);
     const handlerAddProductsCart = (product) =>{
+        
+        
         const hasItem=cartItems.find((i)=> i.product.id===product.id);
-        //console.log(hasItem);
         if (hasItem){
-            // setCartItems([
-            //     ...cartItems.filter((i)=>i.product.id !== product.id),
-            //     {
-            //         product,
-            //         quantity:hasItem.quantity+1,
-            //     }
-            // ])
-            setCartItems(
-                cartItems.map((i)=>{
-                    if (i.product.id===product.id){
-                        i.quantity+=1;
-                    }
-                    return i;
-                })
+            dispatch(
+                {
+                    type:UpdateQuantityProductCart,
+                    payload:product,
+                }
             )
         }else{
-            setCartItems([
-            ...cartItems,
-            {
-                product,
-                quantity:1,
-            }
-        ])
+            dispatch(
+                {
+                    type:AddProductCart,
+                    payload:product
+                }
+            )
         }
     };
 
     const handlerDeleteProductCart = (id)=>{
-        setCartItems([
-            ...cartItems.filter((i)=>i.product.id !== id)
-        ]);
+        dispatch(
+            {
+                type: DeleteProductCart,
+                payload: id
+            }
+        )
     }
 
     return (
